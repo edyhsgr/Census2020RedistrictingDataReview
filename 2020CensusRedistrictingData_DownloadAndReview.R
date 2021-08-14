8c4323dc17a40ca4beb21a4fa463901705e2f8d1
Eddie Hunsinger <43016799+edyhsgr@users.noreply.github.com>
2021-08-14 01:00:42 -0700

    Update 2020CensusRedistrictingData_DownloadAndReview.R

--- 2020CensusRedistrictingData_DownloadAndReview.R
+++ 2020CensusRedistrictingData_DownloadAndReview.R
@@ -1,8 +1,9 @@
 
 ##This is a modified version of the R code that the US Census Bureau provided to download and review the 2020 Census Redistricting Files
+	##It's set to tabulate county total population for each state, and compare that to the US Census Bureau's 2020 Evaluation Estimates by county
+	##My parts of the code (and thus the results) have not been carefully reviewed
 	##Link to the US Census Bureau-provided code: https://www2.census.gov/programs-surveys/decennial/rdo/about/2020-census-program/Phase3/SupportMaterials/2020PL_R_import_scripts.zip 
 	##via https://www.census.gov/programs-surveys/decennial-census/about/rdo/summary-files.html
-	##It's set to tabulate county total population for each state, and compare that to the US Census Bureau's 2020 Evaluation Estimates by county
 	##US Census Bureau's 2020 Evaluation Estimates are accessed via https://www.census.gov/programs-surveys/popest/technical-documentation/research/evaluation-estimates/2020-evaluation-estimates/2010s-cities-and-towns-total.html
 	##
 	##To make the code work, you should be able to simply select-all, and copy and paste into an R command line 
